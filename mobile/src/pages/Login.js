60333d3481f66d146abb95d08e1c93d127a699f1
wjsassi <welson.sassi@hotmail.com>
2020-02-18 19:34:24 -0300

    Login working

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text, TextInput, StyleSheet } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login() {
-    return <View />
+    return (
+    <View style={styles.container}>
+        <Image source={logo} />
+
+        <View style={styles.form}>
+            <Text style={styles.label}>SEU E-MAIL *</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="Seu e-Mail"
+                placeholderTextColor="#999"
+            />
+        </View>
+    </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    form: {
+        alignSelf: 'strech',
+        paddingHorizotal: 30,
+        marginTop: 30,
+    },
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    }
+});
